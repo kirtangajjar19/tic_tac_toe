6a3da54d8695ba6545fda8704ba2083b58494fd9
kirtanmgajjar <kirtanmgajjar@gmail.com>
2025-12-22 20:04:36 +0530

    Update base path in vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './tic_tac_toe/',
+  base: '/tic_tac_toe/', // <-- Use absolute path for GitHub Pages
 })
